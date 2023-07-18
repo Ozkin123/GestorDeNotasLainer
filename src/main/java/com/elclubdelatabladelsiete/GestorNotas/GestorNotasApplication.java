369162d11428d9fdb19cc66fb4d694e0744f4bfd
Oscar <ozkin1vs@hotmail.com>
2023-07-18 11:56:07 -0500

    Primer Commit

---
+++ src/main/java/com/elclubdelatabladelsiete/GestorNotas/GestorNotasApplication.java
@@ -0,0 +1,13 @@
+package com.elclubdelatabladelsiete.GestorNotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elclubdelatabladelsiete/GestorNotas/GestorNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elclubdelatabladelsiete.GestorNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
